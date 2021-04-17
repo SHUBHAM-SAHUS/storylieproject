90ca82bb6731ee9492bf6f1167ba1ebde88a057b
shubham <shubhamsahu1396@gmail.com>
2021-04-17 11:59:44 +0530

    update

--- src/router/routeConfig.js
+++ src/router/routeConfig.js
@@ -1,28 +1,14 @@
-// public routes
-
 import Home from "components/Home";
-
-
- 
 
 //private routes
 
-
 export const publicRoutes = [
-
-
-
-
-    {
-        key: "home",
-        path: "/home",
-        component: Home,
-        exact: true
-      },
-     
-  
+  {
+    key: "home",
+    path: "/",
+    component: Home,
+    exact: true,
+  },
 ];
 
-export const privateRoutes = [
-  
-];
+export const privateRoutes = [];
