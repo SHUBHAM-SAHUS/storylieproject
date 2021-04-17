865e7e3e981df300f8d6996a3b0d85c18271e7b5
navneet <navneet.brinfotech@gmail.com>
2021-04-17 19:11:59 +0530

    sidebar

--- src/components/commoncomponent/HeaderLayout/Header.js
+++ src/components/commoncomponent/HeaderLayout/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import collapsebtn from "../../../assets/images/collapse-btn.png"
 import Untitled from "../../../assets/images/Untitled-1.png"
 import help from "../../../assets/images/icon--help.png"
 import setting from "../../../assets/images/icon-setting.png"
@@ -9,81 +8,42 @@ import dropdownsearch from "../../../assets/images/dropdown-cross-btn.png"
 import logo from "../../../assets/images/logo.png"
 import alert from "../../../assets/images/alert-icon.png"
 import userimages from "../../../assets/images/icon-user.png"
-import { withStyles } from '@material-ui/core/styles';
+import { createMuiTheme, withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
- export const Header = () => {
-         
-//   function CustomizedMenus() {
-//     const [anchorEl, setAnchorEl] = React.useState(null);
-  
-//     const handleClick = (event) => {
-//       setAnchorEl(event.currentTarget);
-//     };
-  
-//     const handleClose = () => {
-//       setAnchorEl(null);
-//     };
-// }
+import MailIcon from '@material-ui/icons/Mail';
+import { CgMenuLeft } from "react-icons/cg";
+import Sidebar from '../Sidebar/Sidebar'
 
-// export const CustomizedMenus=()=> {
-//   const [anchorEl, setAnchorEl] = React.useState(null);
 
-//   const handleClick = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
+ export const Header = () => {    
 
-//   const handleClose = () => {
-//     setAnchorEl(null);
-//   };
-// }
-      
-    
-    // const StyledMenu = withStyles({
-    //     paper: {
-    //       border: '1px solid #d3d4d5',
-    //       marginTop: 130
-    //     },
-    //   })((props) => (
-    //     <Menu
-    //       elevation={0}
-    //       getContentAnchorEl={null}
-    //       anchorOrigin={{
-    //         vertical: 'bottom',
-    //         horizontal: 'center',
-    //       }}
-    //       transformOrigin={{
-    //         vertical: 'top',
-    //         horizontal: 'center',
-    //       }}
-    //       {...props}
-    //     />
-    //   ));
-      
-    //   const StyledMenuItem = withStyles((theme) => ({
-    //     root: {
-    //       '&:focus': {
-    //         backgroundColor: theme.palette.primary.main,
-    //         '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
-    //           color: theme.palette.common.white,
-    //         },
-    //       },
-    //     },
-    //   }))(MenuItem);
-      
-  
-
-    return (
+  return (
     <>
- <nav className="navbar navbar-expand-lg navbar-light shadow" style={{"background":"#ffffff"}}>
-  <div className="container-fluid"> <a id="sidebarCollapse"> <img src={collapsebtn} alt="img" className="mr-4" /> </a> <a href="/"> <img src={logo} alt="logo" /> </a>
-    
+            {/* our html here */}
+            <nav className="navbar navbar-expand-lg navbar-light shadow" style={{"background":"#ffffff"}}>
+  <div className="container-fluid"> <a id="sidebarCollapse"><Sidebar /> </a> <a href="/"> <img src={logo} alt="logo" /> </a>
+  
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <form className="form-inline my-2 my-lg-0 ml-auto search-form">
         <input className="form-control mr-sm-2 ml-auto" type="search" placeholder="Search" aria-label="Search..." />
@@ -91,13 +51,6 @@ import SendIcon from '@material-ui/icons/Send';
       </form>
       <ul className="nav navbar-nav ml-auto">
         <li className="nav-item active dropdown "> <a className="nav-link dropdown-toggle"    aria-haspopup="true" aria-expanded="false"> <img src={dropdownsearch} alt="pic" /> </a> 
-        
-                   
-
-        
-
-
-
           <div className="dropdown-menu createdropdown" aria-labelledby="dropdownMenuLink">
             <ul className="list-unstyled create-list">
               <li><h4>Create</h4></li>
@@ -134,12 +87,7 @@ import SendIcon from '@material-ui/icons/Send';
       </ul>
     </div>
   </div>
-</nav>
-    
-
-    </>
-
-       
-    
-    )
+</nav>  
+</>
+  );
 }
